706ef759b7c18d915b2fd066731b1cb228279329
akatsukac <akatsukac@gmail.com>
2019-05-25 14:08:01 -0500

    Reverted the vertical header changes

--- src/components/shell/header/Header.style.ts
+++ src/components/shell/header/Header.style.ts
@@ -11,36 +11,30 @@ export const LogoImg = styled(Img)`
 
 export const ToolbarContainer = styled(Toolbar)`
   display: flex;
-  align-items: flex-start !important;
-  flex-direction: column;
+  justify-content: space-between;
   color: white;
   padding: 30px !important;
-
 `;
 
 export const AppBarStylings = {
   'border-width': '0px',
   'box-shadow': '0px 0px',
-  'background-color': 'rgba(0,0,0,0.0)',
-  'width': 'inherit',
-  'right': 'inherit'
+  'background-color': 'rgba(0,0,0,0.0)'
 };
 
 export const LinksContainer = styled.div`
   display: flex;
-  flex-direction: column;
-  padding-top: 10px;
 `;
 
 export const LinkContainer = styled.div`
-  padding: 10px 0;
+  padding: 0 30px;
 `;
 
 export const StyledLink = styled(Link)`
   text-decoration: none;
   color: white;
-  font-size: 14px;
-  font-weight: 300;
+  font-size: 18px;
+  font-weight: 700;
 
   :hover {
     border-bottom: 4px solid #D95920;
