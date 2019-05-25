710e4bb6f9a7bac5b7885f8c6e84157ff2d2698e
akatsukac <akatsukac@gmail.com>
2019-05-25 15:41:00 -0500

    Added in all project text content and added in icons

--- src/components/projects/project/Project.tsx
+++ src/components/projects/project/Project.tsx
@@ -1,1 +1,22 @@
-// JUST STATIC CONTENT
+import * as React from 'react';
+import { FunctionComponent } from "react";
+
+const Project: FunctionComponent = ({children}) => {
+    return (
+      <>
+        <div>
+          {children}
+          <span>
+            READ MORE
+          </span>
+          <span>
+            READ LESS
+          </span>
+
+        </div>
+
+      </>
+    );
+};
+
+export default Project;
