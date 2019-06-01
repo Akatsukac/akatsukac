eb46704c22b2db2293bfc317dde2780e41729299
akatsukac <akatsukac@gmail.com>
2019-06-01 01:14:35 -0500

    Updated projects

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -22,24 +22,24 @@ export default class Projects extends React.Component {
             </StyledParagraph>
             <StyledParagraph className="collapsible-content collapsed">
               At Capital One, we have a web application called "Dealer Navigator" that allows auto dealers
-              to fund their customers' auto loans. The document upload section, one of the core services offered,
+              to finance their customers' auto loans. The document upload section, one of the core services offered,
               receives hundreds of thousands of visitors per day and generates over a billion dollars in revenue
-              per year. Unfortunately, the application became a monolith over the years and desperately needed
-              its tech debt addressed.
+              every year. Unfortunately, the application became a monolith over the years and desperately needed
+              a rework.
             </StyledParagraph>
             <StyledParagraph className="collapsible-content collapsed">              
               I led the effort in building an Angular micro frontend with a dedicated backend for frontend 
               for the document upload section. In addition to a normal developer workload, I reviewed pull
-              requests, led architecture discussions, and provided one-on-one mentorship sessions. By the end of 
+              requests, guided architecture discussions, and provided one-on-one mentorship sessions. By the end of 
               the project, we achieved our goal of having a modular and refactored codebase for our 
               frontend and backend.
             </StyledParagraph>
             <StyledParagraph className="collapsible-content collapsed">
               Beyond dockerization and migrating code out of the monolith, I made many technical improvements.
-              I hosted the dockerized containers on Amazon Web Services ECS cluster, automated the run of perf
+              I hosted the dockerized containers on an Amazon Web Services ECS cluster, automated the run of perf
               and functional tests before each release, and added route guards to prevent URL tampering. I also
-              made many code improvements such as moving all application logic to the BFF layer, updating 
-              deprecated dependencies, and improving test coverage that surpassed company standards.
+              moved all application logic to the BFF layer, updated deprecated dependencies, and improved test 
+              coverage to surpass company standards.
             </StyledParagraph>
           </Project>
           <Project>
@@ -52,17 +52,15 @@ export default class Projects extends React.Component {
               mail and fax.
             </StyledParagraph>
             <StyledParagraph className="collapsible-content collapsed">
-              I built a document previewer with PDFJS, integrated NGRX for maintaining component
-              state across different pages, and built banners and tables to support dealer funding 
-              requirements. In order to fully leverage all of PDFJS's features, I built a Lambda function
+              I integrated NGRX for maintaining component state across different pages, built collapsible banners 
+              for previewing uploaded documents, and tables to clearly communicate dealer funding 
+              requirements. In order to fully leverage all of PDFJS's features, I also built a Lambda function
               on Amazon Web Services that created a PDF copy of every image uploaded to S3. The document previewer
               could then leverage the PDF copy, which provided all of PDFJS's features across all browser types.
             </StyledParagraph>
             <StyledParagraph className="collapsible-content collapsed">
               Through this project, I learned how to build responsive pages that could work across all 
               modern browsers. I had to learn about break points, polyfills, and the wonders of caniuse.com.
-              Sauce Labs in conjunction with the Selenium web driver allowed me to quickly test my pages
-              across supported browsers very quickly.
             </StyledParagraph>
           </Project>
           <Project>
@@ -80,9 +78,8 @@ export default class Projects extends React.Component {
               application parameters, and custom requirements created by funders. 
             </StyledParagraph>
             <StyledParagraph className="collapsible-content collapsed">
-              Dealers responded to these changes with overwhelming feedback and helped to
-              improve the number of "clean deals" (deals that provided all required documentation on the
-              first try).
+              Dealers responded to these changes with overwhelmingly positive feedback and the number of 
+              "clean deals" (deals that provided all required documentation on the first try) improved drastically.
             </StyledParagraph>
           </Project>
           <Project>
@@ -95,17 +92,16 @@ export default class Projects extends React.Component {
               saving $6 million annually.
             </StyledParagraph>
             <StyledParagraph className="collapsible-content collapsed">
-              I built the image preprocessing Python microservice. The dockerized image preprocessing 
-              tool leveraged OpenCV to grayscale images, convert files to jpeg, and perform content isolation
-              transformations to increase accuracy. We deployed the container on an Amazon Web Services 
-              ECS cluster. When an upload occurred, our orchestrator placed a message in
+              For the pipeline, I built an image preprocessing Python microservice. The dockerized image 
+              preprocessing tool leveraged OpenCV to grayscale images, convert files to jpeg, and perform 
+              content isolation transformations to increase accuracy. We deployed the container on an 
+              Amazon Web Services ECS cluster. When an upload occurred, our orchestrator placed a message in
               an SQS queue, which my preprocessing service consumed. 
             </StyledParagraph>
             <StyledParagraph className="collapsible-content collapsed">
               Upon completion, my service saved the processed image in an S3 bucket and placed
               a message for the next microservice in our pipeline to consume.  In the event of failure,
-              we retried up to three times and if the failure still occurred, we placed a message
-              in a dead letter queue for manual review.
+              we retried up to three times and before placing a message in a dead letter queue for manual review.
             </StyledParagraph>
             <StyledParagraph className="collapsible-content collapsed">
               I also built an Angular 4 re-classification tool to help with the retraining of our 
