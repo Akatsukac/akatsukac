f0bb4584439a9639daa7744b82e80ca65aa2095b
akatsukac <akatsukac@gmail.com>
2019-06-01 01:45:42 -0500

    Fixed up projects section

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -44,7 +44,7 @@ export default class Projects extends React.Component {
           </Project>
           <Project>
             <StyledProjectHeader>
-              Piloting A New Experience
+              Reimagining The Customer Experience
             </StyledProjectHeader>
             <StyledParagraph>
               I rebuilt a business-critical Angular web app with loan funding requirements, document classification,
@@ -55,12 +55,11 @@ export default class Projects extends React.Component {
               I integrated NGRX for maintaining component state across different pages, built collapsible banners 
               for previewing uploaded documents, and tables to clearly communicate dealer funding 
               requirements. In order to fully leverage all of PDFJS's features, I also built a Lambda function
-              on Amazon Web Services that created a PDF copy of every image uploaded to S3. The document previewer
-              could then leverage the PDF copy, which provided all of PDFJS's features across all browser types.
+              on Amazon Web Services that created a PDF copy of every image uploaded to S3.
             </StyledParagraph>
             <StyledParagraph className="collapsible-content collapsed">
               Through this project, I learned how to build responsive pages that could work across all 
-              modern browsers. I had to learn about break points, polyfills, and the wonders of caniuse.com.
+              modern browsers. I had to learn about breakpoints, polyfills, and the wonders of caniuse.com.
             </StyledParagraph>
           </Project>
           <Project>
@@ -68,18 +67,17 @@ export default class Projects extends React.Component {
               Clearing Up The Confusion
             </StyledProjectHeader>
             <StyledParagraph>
-              I architected and developed a Java Spring Boot REST API that allows Capital One funders
-              to specify loan funding requirements directly to customers, reducing the number of 
-              applications with missing documents by 55%.
+              I architected and developed a Java Spring Boot REST API that provides application specific
+              funding requirements, reducing the number of applications with missing documents by 55%.
             </StyledParagraph>
             <StyledParagraph className="collapsible-content collapsed">
-              I leveraged the Builder pattern to program business logic into readable and maintainable classes.
-              This allowed me to separate the logic for universal requirements, special requirements due to 
-              application parameters, and custom requirements created by funders. 
+              I leveraged the Builder design pattern to program business logic into readable and maintainable classes.
+              This allowed me to modularize the logic for including universal requirements, special requirements due to 
+              application parameters, and custom requirements from funders. 
             </StyledParagraph>
             <StyledParagraph className="collapsible-content collapsed">
               Dealers responded to these changes with overwhelmingly positive feedback and the number of 
-              "clean deals" (deals that provided all required documentation on the first try) improved drastically.
+              "clean deals" (deals that provided all required documentation on the first try) increased drastically.
             </StyledParagraph>
           </Project>
           <Project>
@@ -88,26 +86,27 @@ export default class Projects extends React.Component {
             </StyledProjectHeader>
             <StyledParagraph>
               I replaced a manual document classification process with an event-driven Python microservice
-              pipeline on AWS that classifies 1 million documents per day in less than 10 seconds per page,
-              saving $6 million annually.
+              pipeline on Amazon Web Services that classifies 1 million documents per day in less than 10 
+              seconds per page, saving $6 million annually.
             </StyledParagraph>
             <StyledParagraph className="collapsible-content collapsed">
               For the pipeline, I built an image preprocessing Python microservice. The dockerized image 
               preprocessing tool leveraged OpenCV to grayscale images, convert files to jpeg, and perform 
-              content isolation transformations to increase accuracy. We deployed the container on an 
-              Amazon Web Services ECS cluster. When an upload occurred, our orchestrator placed a message in
-              an SQS queue, which my preprocessing service consumed. 
+              content isolation transformations to increase accuracy. I deployed the container on an 
+              Amazon Web Services ECS cluster.
             </StyledParagraph>
             <StyledParagraph className="collapsible-content collapsed">
-              Upon completion, my service saved the processed image in an S3 bucket and placed
-              a message for the next microservice in our pipeline to consume.  In the event of failure,
-              we retried up to three times and before placing a message in a dead letter queue for manual review.
+              When an upload occurs, the orchestrator places a message in an SQS queue, which my preprocessing
+              service consumes. Upon completion, my service saves the processed image in an S3 bucket and places
+              a message for the next microservice in the pipeline to consume.  In the event of failure,
+              the service retries up to three times before placing a message in a dead letter queue for 
+              manual review.
             </StyledParagraph>
             <StyledParagraph className="collapsible-content collapsed">
-              I also built an Angular 4 re-classification tool to help with the retraining of our 
-              classification model.  Users could see uploaded images alongside their classification.
-              The user could then select the correct classification and these new labels could then 
-              contribute directly to our next model retraining to help improve accuracy.
+              I also built an Angular 4 GUI to help with the retraining of the classification model. The GUI allows 
+              users to view uploaded images next to their classification. If the user identifies a misclassification,
+              he or she can select the correct label from a dropdown. Reclassification data feeds directly into 
+              retraining data sets, which help to improve classification accuracy.
             </StyledParagraph>
           </Project>
         </StyledProjectsContainer>
