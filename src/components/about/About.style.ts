7f7cc5429b20eea4aec44b4d8062e1f511a07050
akatsukac <akatsukac@gmail.com>
2019-06-01 02:39:03 -0500

    Fixed up the git icon

--- src/components/about/About.style.ts
+++ src/components/about/About.style.ts
@@ -1,30 +1,26 @@
 import styled from 'styled-components';
 import Img from 'gatsby-image';
 
-export const StyledAboutContainer = styled.div`
-    padding: 50px 0;
-    text-align: center;
-`;
-
 export const StyledAnchor = styled.a`
     text-decoration: none;
 `;
 
-export const StyledContentContainer = styled.div`
+export const StyledAboutContainer = styled.div`
+  padding: 50px 0;
+  text-align: center;
+  min-height: calc(100vh - 115px);
     display: flex;
     justify-content: space-evenly;
-    margin-bottom: 50px;
 `;
 
 export const LogoImg = styled(Img)`
-    min-height: 400px;
-    min-width: 400px;
+    min-height: 75vh;
+    min-width: 60vh;
 `;
 
 export const StyledParagraphContainer = styled.div`
     max-width: 40%;
     margin: 0;
-    text-align: initial;
 `;
 
 export const StyledParagraph = styled.p`
@@ -32,7 +28,7 @@ export const StyledParagraph = styled.p`
     &:first-child, &:last-child {
         margin: 0;
     }
-
+    text-align: initial;
     font-size: 24px;
     font-weight: 400;
     color: #2D3739;
