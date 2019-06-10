115753711e0201f43f3e0f4fb72e87f0fa75fdbe
akatsukac <akatsukac@gmail.com>
2019-06-10 03:31:34 -0500

    Fixed up 404 page

--- src/components/shell/Shell.tsx
+++ src/components/shell/Shell.tsx
@@ -1,18 +1,27 @@
 import * as React from 'react';
 import Footer from './footer/Footer';
 import Header from './header/Header';
+import { 
+  ChildrenContainer,
+  ShellContainer
+} from './Shell.style';
 import { FunctionComponent } from 'react';
 
 const Shell: FunctionComponent = ({children}) => {
-    return (
-      <>
-        <Header>
-        </Header>
+  if (children === null || children[1] === null) {
+    console.error("No children elements passed!");
+  }
+  return (
+    <ShellContainer className={children[1].props.className}>
+      <Header>
+      </Header>
+      <ChildrenContainer>
         {children}
-        <Footer>        
-        </Footer>
-      </>
-    );
+      </ChildrenContainer>
+      <Footer>        
+      </Footer>
+    </ShellContainer>
+  );
 };
 
 export default Shell;
